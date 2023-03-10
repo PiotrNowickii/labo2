44599cffc87ee9292e116ecbc3882242332d3a0a
PiotrNowickii <s24667@pjwstk.edu.pl>
2023-03-10 18:44:37 +0100

    Edited main file

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
+import webbrowser
 
-
-def print_hi(name):
-
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+pageUrl = 'https:\\github.com'
+webbrowser.open(pageUrl)
